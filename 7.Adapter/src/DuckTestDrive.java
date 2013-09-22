55a0649532d421872495ff18fd6822716453b42b
Klyuchevsky Eugene <juhn@bk.ru>
2013-09-22 16:58:59 +0700

    Add adapter pattern.

---
+++ 7.Adapter/src/DuckTestDrive.java
@@ -0,0 +1,26 @@
+import duck.Duck;
+import duck.MallardDuck;
+import turkey.WildTurkey;
+
+public class DuckTestDrive {
+    public static void main(String[] args) {
+        MallardDuck duck = new MallardDuck();
+        WildTurkey turkey = new WildTurkey();
+        Duck turkeyAdapter = new TurkeyAdapter(turkey);
+
+        System.out.println("The turkey says...");
+        turkey.gobble();
+        turkey.fly();
+
+        System.out.println("\nThe duck says...");
+        testDuck(duck);
+
+        System.out.println("\nThe turkey adapter says...");
+        testDuck(turkeyAdapter);
+    }
+
+    static void testDuck(Duck duck) {
+        duck.quack();
+        duck.fly();
+    }
+}
---
+++ 7.Adapter/src/TurkeyAdapter.java
@@ -0,0 +1,20 @@
+import duck.Duck;
+import turkey.Turkey;
+
+public class TurkeyAdapter implements Duck {
+    Turkey turkey;
+
+    public TurkeyAdapter(Turkey turkey) {
+        this.turkey = turkey;
+    }
+
+    public void quack() {
+        turkey.gobble();
+    }
+
+    public void fly() {
+        for (int i = 0; i < 5; i++) {
+            turkey.fly();
+        }
+    }
+}
