85d078a62a8cf969c25f90b11ea8d920e1e1a4c9
Klyuchevsky Eugene <juhn@bk.ru>
2013-09-30 01:10:27 +0700

    Add state pattern.

---
+++ 11.State/src/GumballMachine.java
@@ -0,0 +1,61 @@
+public class GumballMachine {
+    State soldOutState;
+    State noQuarterState;
+    State hasQuarterState;
+    State soldState;
+
+    State state = soldOutState;
+    int count = 0;
+
+    public GumballMachine(int numberGumballs) {
+        soldOutState = new SoldOutState(this);
+        noQuarterState = new NoQuarterState(this);
+        hasQuarterState = new HasQuarterState(this);
+        soldState = new SoldState(this);
+        this.count = numberGumballs;
+        if (numberGumballs > 0) {
+            state = noQuarterState;
+        }
+    }
+
+    public void insertQuarter() {
+        state.insertQuarter();
+    }
+
+    public void ejectQuarter() {
+        state.ejectQuarter();
+    }
+
+    public void turnCrank() {
+        state.turnCrank();
+        state.dispense();
+    }
+
+    void releaseBall() {
+        System.out.println("A gumball comes rolling out the slot...");
+        if (count != 0) {
+            count = count - 1;
+        }
+    }
+
+
+    void setState(State state) {
+        this.state = state;
+    }
+
+    State getSoldOutState() {
+        return soldOutState;
+    }
+
+    State getNoQuarterState() {
+        return noQuarterState;
+    }
+
+    State getHasQuarterState() {
+        return hasQuarterState;
+    }
+
+    State getSoldState() {
+        return soldState;
+    }
+}
