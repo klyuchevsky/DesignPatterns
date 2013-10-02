0d8213b67aad42ccb62c46eacec4397002bcbc06
Klyuchevsky Eugene <juhn@bk.ru>
2013-10-02 23:17:33 +0700

    Add iterator pattern.

---
+++ 10.Iterator and Composite/src/DinerMenuIterator.java
@@ -0,0 +1,16 @@
+public class DinerMenuIterator implements Iterator {
+    MenuItem[] items;
+    int position = 0;
+
+    public Object next() {
+        MenuItem menuItem = items[position];
+        position++;
+        return menuItem;
+    }
+
+    public boolean hasNext() {
+        if (position >= items.length || items[position] == null) {
+            return false;
+        } else return true;
+    }
+}
---
+++ 10.Iterator and Composite/src/Waitress.java
@@ -0,0 +1,13 @@
+public class Waitress {
+    PancakeHouseMenu pancakeHouseMenu;
+    DinerMenu dinerMenu;
+
+    public Waitress(PancakeHouseMenu pancakeHouseMenu, DinerMenu dinerMenu) {
+        this.pancakeHouseMenu = pancakeHouseMenu;
+        this.dinerMenu = dinerMenu;
+    }
+
+    public void printMenu() {
+//        Iterator
+    }
+}
